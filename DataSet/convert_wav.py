6a406f18ccf7df36e5d9150fc18a702bdc6d116f
ybc9977 <ybc9977@gmail.com>
2019-05-01 20:16:24 +0800

    rename DataSet dir

--- DataSet/convert_wav.py
+++ DataSet/convert_wav.py
@@ -3,5 +3,5 @@ import os.path as path
 for cdir, dirs, files in os.walk('.'):
     for f in files:
         spl = path.splitext(f)
-        if(spl[1] == '.mid'):
+        if spl[1] == '.mid':
             subprocess.run(['timidity', path.abspath(os.path.join(cdir, f)), '-Ow', '-o', path.abspath(path.join(cdir, spl[0] + '.wav'))])
