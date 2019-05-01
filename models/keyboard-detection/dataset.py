b6cd5f16ff8a170e11bfd73e86e50aebaa7ba6af
ppfish.45@gmail.com <ppfish.45@gmail.com>
2019-05-01 12:38:51 +0800

    Update dataset.py

--- models/keyboard-detection/dataset.py
+++ models/keyboard-detection/dataset.py
@@ -57,20 +57,38 @@ def load_all_data():
     for x in X_path:
         print('# of ' + x + ': ' + str(len(X_path[x])))
 
-def show_labelled_image(img, pts):
+def rotate_image(img, image_size=(224, 224), dir=0):
+    ret = copy.copy(img)
+    H = cv2.getRotationMatrix2D((image_size[0] / 2, image_size[1] / 2), -90, 1)
+    for i in range(dir):
+        ret = cv2.warpAffine(ret, H, (ret.shape[1], ret.shape[0]))
+    return ret
+
+def rotate_coordinate(pts, image_size=(224, 224), dir=0):
+    ret = copy.copy(pts)
+    for i in range(dir):
+        ret[:, [0, 1]] = ret[:, [1, 0]]
+        ret[:, 0] = image_size[1] - 1 - ret[:, 0]
+    return ret
+        
+def show_labelled_image(img, pts, dir=0):
     if isinstance(img, str):
         img = cv2.imread(img)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    for k, p in enumerate(pts):
-        cv2.circle(img, (int(p[0]), int(p[1])), 2, (255 * (k % 3 == 0), 255 * (k % 2 == 0), 255), 2)
-    return img
+    _img = copy.copy(img)
+    _img = rotate_image(_img, dir=dir)
+    _pts = rotate_coordinate(pts, dir=dir)
+    for k, p in enumerate(_pts):
+        cv2.circle(_img, (int(p[0]), int(p[1])), 2, (255 * (k % 3 == 0), 255 * (k % 2 == 0), 255), 2)
+    return _img
 
-def get_sample(type='train', img=True):
+def get_sample(type='train', img=True, dir=0, img_size=(640, 360)):
     ind = random.randint(0, len(X_path[f'X_{type}']))
     path = X_path[f'X_{type}'][ind]
     pts = y[f'y_{type}'][ind]
     if img:
         image = cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2RGB)
+        image = cv2.resize(image, img_size, interpolation=cv2.INTER_CUBIC)
         return (image, pts)
     else:
         return (path, pts)
@@ -83,7 +101,7 @@ Image format: NCHW
 '''
 
 class data_batch():
-    def __init__(self, type='train', batch_size=64, image_size=(224, 224), NCHW=True, max_num=-1):
+    def __init__(self, type='train', batch_size=64, image_size=(224, 224), NCHW=True, max_num=-1, random_dir=False):
         self.type = type
         self.batch_size = batch_size
         self.image_size = image_size
@@ -111,7 +129,16 @@ class data_batch():
         if self.NCHW:
             X_return = np.array(np.transpose(X_return, (0, 3, 1, 2))) # convert to NCHW
         y_return = copy.copy(y[f'y_{self.type}'][start : end])
+        # resize image
         y_return *= [self.image_size[0] / 640.0, self.image_size[1] / 360.0]
+
+        # randomly rotate images and coordinates
+        if random_dir:
+            for i in range(batch_size):
+                dir = random.randint(0, 3)
+                X_return[i] = rotate_image(X_return[i], dir=dir)
+                y_return[i] = rotate_coordinate(y_return[i], dir=dir)
+
         self.index += 1
         return (np.array(X_return), y_return)
 
