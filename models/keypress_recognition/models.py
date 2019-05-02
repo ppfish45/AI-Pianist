57fcfbb3b5e1adf2ef6a0a56cb287b06cdd53add
fhfuih <sam.zyhuang@outlook.com>
2019-05-02 19:01:30 +0800

    Make keypress model adapt to the model wrapper API

--- models/keypress_recognition/models.py
+++ models/keypress_recognition/models.py
@@ -1,12 +1,12 @@
 import torch
-from ..model_factory import ModelWrapper
+from ..model_wrapper import ModelWrapper
+
 
 class Flatten(torch.nn.Module):
     def forward(self, x):
         batch_size = x.shape[0]
         return x.view(batch_size, -1)
 
-loss_fn = torch.nn.BCELoss()
 
 white_key_struct = torch.nn.Sequential(
     torch.nn.Conv2d(1, 16, 3, padding=1),
@@ -30,15 +30,12 @@ white_key_struct = torch.nn.Sequential(
 )
 
 
-white_key_optim = torch.optim.Adam(white_key_struct.parameters(), lr=1e-4)
-white_key_scheduler = torch.optim.lr_scheduler.StepLR(white_key_optim, step_size=10, gamma=0.05)
-
 white_key_model = ModelWrapper(
     white_key_struct,
-    loss_fn,
-    white_key_optim,
-    white_key_scheduler
+    torch.nn.BCELoss,
+    torch.optim.Adam
 )
+
 
 black_key_struct = torch.nn.Sequential(
     torch.nn.Conv2d(1, 16, 3, padding=1),
@@ -61,12 +58,9 @@ black_key_struct = torch.nn.Sequential(
     torch.nn.Sigmoid()
 )
 
-black_key_optim = torch.optim.Adam(black_key_struct.parameters(), lr=1e-4)
-black_key_scheduler = torch.optim.lr_scheduler.StepLR(black_key_optim, step_size=10, gamma=0.05)
 
 black_key_model = ModelWrapper(
     black_key_struct,
-    loss_fn,
-    black_key_optim,
-    black_key_scheduler
+    torch.nn.BCELoss,
+    torch.optim.Adam
 )
