d94d506d7ba95fa1928d23446726b55272e1361a
fhfuih <sam.zyhuang@outlook.com>
2019-05-03 00:15:56 +0800

    Refactor model.py

--- models/keypress_recognition/models.py
+++ models/keypress_recognition/models.py
@@ -9,10 +9,12 @@ import torch.nn as nn
 import torch.optim as optim
 
 from ..model_wrapper import ModelWrapper
-from .separate import white_key_height, white_key_width, black_key_width, black_key_height, img_height, img_width
+from .separate import white_key_height, white_key_width, black_key_width, black_key_height, img_height, img_width, white_key_width_bundle, black_key_width_bundle
 
 white_fc_in = (white_key_width // 2 // 2) * (white_key_height // 2 // 2) * 32
 black_fc_in = (black_key_width // 2 // 2) * (black_key_height // 2 // 2) * 32
+w_bundle_fc_in = (white_key_width_bundle // 2 // 2) * (white_key_height // 2 // 2) * 32
+b_bundle_fc_in = (black_key_width_bundle // 2 // 2) * (black_key_height // 2 // 2) * 32
 all_fc_in = (img_width // 2 // 2 // 2 // 2) * (img_height // 2 // 2 // 2 // 2) * 32
 
 
@@ -24,91 +26,110 @@ class Flatten(torch.nn.Module):
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-white_key_struct = torch.nn.Sequential(
-    torch.nn.Conv2d(1, 16, 3, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.BatchNorm2d(16),
-    torch.nn.Conv2d(16, 16, 3, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.BatchNorm2d(16),
-    torch.nn.MaxPool2d(2),
-    torch.nn.Conv2d(16, 32, 3, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.BatchNorm2d(16),
-    torch.nn.Conv2d(32, 32, 3, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.BatchNorm2d(32),
-    torch.nn.MaxPool2d(2),
-    Flatten(),
-    torch.nn.Linear(white_fc_in, 512),
-    torch.nn.Linear(512, 1),
-    torch.nn.Sigmoid()
-)
+def get_2layer_model(linear_in):
+    model_struct = torch.nn.Sequential(
+        torch.nn.Conv2d(1, 16, 3, padding=1),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(16),
+        torch.nn.Conv2d(16, 16, 3, padding=1),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(16),
+        torch.nn.MaxPool2d(2),
+        torch.nn.Conv2d(16, 32, 3, padding=1),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(32),
+        torch.nn.Conv2d(32, 32, 3, padding=1),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(32),
+        torch.nn.MaxPool2d(2),
+        Flatten(),
+        torch.nn.Linear(linear_in, 512),
+        torch.nn.Linear(512, 1),
+        torch.nn.Sigmoid()
+    )
+    return ModelWrapper(
+        model_struct,
+        torch.nn.BCELoss,
+        torch.optim.Adam
+    )
 
-white_key_model = ModelWrapper(
-    white_key_struct,
-    torch.nn.BCELoss,
-    torch.optim.Adam
-)
+def get_3layer_model(linear_in):
+    model_struct = torch.nn.Sequential(
+        torch.nn.Conv2d(1, 16, 3, padding=1),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(16),
+        torch.nn.Conv2d(16, 16, 3, padding=1),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(16),
+        torch.nn.MaxPool2d(2),
+        torch.nn.Conv2d(16, 32, 3, padding=1),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(32),
+        torch.nn.Conv2d(32, 32, 3, padding=1),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(32),
+        torch.nn.MaxPool2d(2),
+        torch.nn.Conv2d(32, 64, 3, padding=1),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(64),
+        torch.nn.Conv2d(64, 64, 3, padding=1),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(64),
+        torch.nn.MaxPool2d(2),
+        Flatten(),
+        torch.nn.Linear(linear_in, 512),
+        torch.nn.Linear(512, 1),
+        torch.nn.Sigmoid()
+    )
+    return ModelWrapper(
+        model_struct,
+        torch.nn.BCELoss,
+        torch.optim.Adam
+    )
 
-black_key_struct = torch.nn.Sequential(
-    torch.nn.Conv2d(1, 16, 3, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.BatchNorm2d(16),
-    torch.nn.Conv2d(16, 16, 3, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.BatchNorm2d(16),
-    torch.nn.MaxPool2d(2),
-    torch.nn.Conv2d(16, 32, 3, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.BatchNorm2d(16),
-    torch.nn.Conv2d(32, 32, 3, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.BatchNorm2d(32),
-    torch.nn.MaxPool2d(2),
-    Flatten(),
-    torch.nn.Linear(black_fc_in, 512),
-    torch.nn.Linear(512, 1),
-    torch.nn.Sigmoid()
-)
+def get_full_model(linear_in):
+    model_struct = torch.nn.Sequential(
+        torch.nn.Conv2d(1, 16, 3, padding=1),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(16),
+        torch.nn.Conv2d(16, 16, 3, padding=1),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(16),
+        torch.nn.MaxPool2d(2),
+        torch.nn.Conv2d(16, 32, 3, padding=1),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(32),
+        torch.nn.Conv2d(32, 32, 3, padding=1),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(32),
+        torch.nn.MaxPool2d(2),
+        torch.nn.Conv2d(32, 64, 3, padding=1),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(64),
+        torch.nn.Conv2d(64, 64, 3, padding=1),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(64),
+        torch.nn.MaxPool2d(2),
+        torch.nn.Conv2d(64, 128, 3, padding=1),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(128),
+        torch.nn.Conv2d(128, 128, 3, padding=1),
+        torch.nn.ReLU(),
+        torch.nn.BatchNorm2d(128),
+        torch.nn.MaxPool2d(2),
+        Flatten(),
+        torch.nn.Linear(linear_in, 1024),
+        torch.nn.Linear(1024, 88),
+        torch.nn.Sigmoid()
+    )
+    return ModelWrapper(
+        model_struct,
+        torch.nn.BCELoss,
+        torch.optim.Adam
+    )
 
-black_key_model = ModelWrapper(
-    black_key_struct,
-    torch.nn.BCELoss,
-    torch.optim.Adam
-)
-
-keyboard_model = torch.nn.Sequential(
-    torch.nn.Conv2d(1, 16, 3, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.BatchNorm2d(16),
-    torch.nn.Conv2d(16, 16, 3, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.BatchNorm2d(16),
-    torch.nn.MaxPool2d(2),
-    torch.nn.Conv2d(16, 32, 3, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.BatchNorm2d(32),
-    torch.nn.Conv2d(32, 32, 3, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.BatchNorm2d(32),
-    torch.nn.MaxPool2d(2),
-    torch.nn.Conv2d(32, 64, 3, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.BatchNorm2d(64),
-    torch.nn.Conv2d(64, 64, 3, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.BatchNorm2d(64),
-    torch.nn.MaxPool2d(2),
-    torch.nn.Conv2d(64, 128, 3, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.BatchNorm2d(128),
-    torch.nn.Conv2d(128, 128, 3, padding=1),
-    torch.nn.ReLU(),
-    torch.nn.BatchNorm2d(128),
-    torch.nn.MaxPool2d(2),
-    Flatten(),
-    torch.nn.Linear(all_fc_in, 1024),
-    torch.nn.Linear(1024, 88),
-    torch.nn.Sigmoid()
-)
+white_key_model = get_2layer_model(white_fc_in)
+black_key_model = get_2layer_model(black_fc_in)
+white_bundle_model = get_3layer_model(w_bundle_fc_in)
+black_bundle_model = get_3layer_model(b_bundle_fc_in)
+keyboard_model = get_full_model(all_fc_in)
